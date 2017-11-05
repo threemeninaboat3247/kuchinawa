4a574bfa3d7c45daceeabe2fdf16968ec269a835
Yuki Arai <threemeninaboat3247@gmail.com>
2017-11-06 00:42:51 +0900

    Preparation for version 0.2 release
    1. Included .png file in Icons folder
    2. Fixed run_sample method

--- kuchinawa/__init__.py
+++ kuchinawa/__init__.py
@@ -4,18 +4,28 @@ Created on Mon Oct 23 03:24:37 2017
 
 @author: Yuki
 """
+import os,sys
+
 from kuchinawa.Compile import compileUi
 from kuchinawa.Thread import Main
 
+import pdb
+pdb.set_trace()
 ENTRYPOINT=__path__[0]
-ICONPATH=ENTRYPOINT+'\\Icons\\logo.png'
+ICONPATH=os.path.join(ENTRYPOINT,'Icons','logo.png')
 KUCHINAWA='Kuchinawa'
 
-def run_sample():
-    from PyQt5.QtWidgets import QApplication
-    import sys,multiprocessing
+#change the multiprocessing's context to 'spawn'
+try:
+    import multiprocessing
     multiprocessing.set_start_method('spawn')
+except:
+    print('The context of multiprocessing is already set.')
+
+def run_sample():
+    '''Run a sample program'''
+    from PyQt5.QtWidgets import QApplication
     from kuchinawa.Examples import SinCos
     app = QApplication([])
     s=SinCos.Sample()
-    sys.exit(app.exec_())    
+    sys.exit(app.exec_())
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ long_description = 'This package help you to create a control program for measur
 
 setup(
     name  = 'kuchinawa',
-    version = '0.0',
+    version = '0.2',
     description = 'Support for creating a control program for measurement instruments',
     long_description = long_description,
     license = 'MIT',
@@ -20,6 +20,10 @@ setup(
     url = 'https://github.com/threemeninaboat3247/kuchinawa',
     keywords = 'measurement',
     packages = find_packages(),
+    package_data={
+        # If any package contains *.txt or *.rst files, include them:
+        'raipy': ['Icons/*.png']
+    },
     install_requires = ['pyqtgraph>=0.10.0','pandas'],
     classifiers = [
       'Programming Language :: Python :: 3',
